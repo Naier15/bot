deb5e918e3ec06bfb43ead0fea38b9ad725c996e
Roman <grv1510@mail.ru>
2025-01-27 03:08:16 +1000

    added emoji

--- app/router.py
+++ app/router.py
@@ -1,53 +1,33 @@
+from collections import deque
 from aiogram import Router, F, types
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-import asyncio
+import emoji
 
-from app.app import Context, App
+from app.app import Context, App, Page
 from app import text
-
 
 
 router = Router()
 context = Context()
 
-MENU = 'Меню'
-FLATS = 'Каталог квартир'
-OFFICES = 'Помещения для бизнеса'
-PROFILE = 'Мой профиль'
-PHOTO = 'Фотоотчеты'
-HELP = 'Помощь'
-BACK = 'Назад'
-
-
-      
-class Page(StatesGroup):
-    menu = State()
-    none = State()
 
 @router.message(CommandStart())
 async def start(msg: types.Message, state: FSMContext):
     btn = [[types.KeyboardButton(text='Разрешить', request_contact=True)]]
     markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
     await msg.answer(text.introduction, reply_markup=markup)
-    await state.set_state(Page.menu)
+    await App.set_state(Page.menu, state)
 
-@router.message(F.contact, Page.menu)
-async def get_contact(msg: types.Message, state: FSMContext):
-    contact = msg.contact
-    await msg.answer(f'Сохранили ваш номер {contact}', reply_markup=App.menu())
-    await state.set_state(Page.none)
+# @router.message(Page.menu, F.contact)
+# async def get_contact(msg: types.Message):
+#     contact = msg.contact
+#     await get_menu(msg)
 
-@router.message(Page.menu)
-async def get_menu(msg: types.Message, state: FSMContext):
-    await msg.answer(f'Меню:', reply_markup=App.menu())
-    await state.set_state(Page.none)
-
-
-@router.message(F.text == FLATS)
-async def flats(msg: types.Message):
-    btn = [[types.KeyboardButton(text='Назад')]]
+@router.message(F.text == App.FLATS)
+async def flats(msg: types.Message, state: FSMContext):
+    btn = [[types.KeyboardButton(text=App.BACK)]]
     markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
     await msg.answer((
         'Каталог квартир интегрирован с сайтами застройщиков: актуальное наличие, цены застройщиков, бесплатное сопровождение от bashni.pro\n'
@@ -62,10 +42,11 @@ async def flats(msg: types.Message):
         'Владивосток (https://bashni.pro/property/vladivostok/flats/)\n'
         'Уссурийск (https://bashni.pro/property/ussurijsk/flats/ )'
     ), reply_markup=markup)
+    await App.set_state(Page.flats, state)
 
-@router.message(F.text == OFFICES)
-async def offices(msg: types.Message):
-    btn = [[types.KeyboardButton(text='Назад')]]
+@router.message(F.text == App.OFFICES)
+async def offices(msg: types.Message, state: FSMContext):
+    btn = [[types.KeyboardButton(text=App.BACK)]]
     markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
     await msg.answer((
         'В каталоге представлены коммерческие помещения для ведения бизнеса в новостройках.\n'
@@ -79,10 +60,11 @@ async def offices(msg: types.Message):
         'Перейти в каталог помещений\n'
         'Владивосток (https://bashni.pro/property/vladivostok/commercial/ )'
     ), reply_markup=markup)
+    await App.set_state(Page.offices, state)
 
-@router.message(F.text == PROFILE)
-async def profile(msg: types.Message):
-    btn = [[types.KeyboardButton(text='Редактировать анкету'), types.KeyboardButton(text='Назад')]]
+@router.message(F.text == App.PROFILE)
+async def profile(msg: types.Message, state: FSMContext):
+    btn = [[types.KeyboardButton(text='Редактировать анкету')], [types.KeyboardButton(text=App.BACK)]]
     markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
     await msg.answer((
         '[эмодзи] Телефон\n'
@@ -90,18 +72,30 @@ async def profile(msg: types.Message):
         '[эмодзи] Email\n'
         '[эмодзи] Мой дом'
     ), reply_markup=markup)
+    await App.set_state(Page.profile, state)
 
-@router.message(F.text == HELP)
-async def help(msg: types.Message):
-    btn = [[types.KeyboardButton(text='Назад')]]
+@router.message(F.text == App.SUBSCRIPTION)
+async def subscription(msg: types.Message, state: FSMContext):
+    btn = [[types.KeyboardButton(text='Новая подписка')], [types.KeyboardButton(text=App.BACK)]]
     markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
     await msg.answer('Бот, в котором можно задать вопросы менеджеру', reply_markup=markup)
+    await App.set_state(Page.subscription, state)
 
-@router.message(F.text == BACK)
-async def back_to_menu(msg: types.Message, state: FSMContext):
-    await msg.answer(f'Меню:', reply_markup=App.menu())
-    await state.set_state(Page.menu)
+@router.message(F.text == App.HELP)
+async def help(msg: types.Message, state: FSMContext):
+    btn = [[types.KeyboardButton(text=App.BACK)]]
+    markup = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выберете действие:')
+    await msg.answer('Бот, в котором можно задать вопросы менеджеру', reply_markup=markup)
+    await App.set_state(Page.help, state)
 
+@router.message(F.text == App.BACK)
+async def back(msg: types.Message, state: FSMContext):
+    await App.go_back(state)
+    await get_menu(msg)
+
+@router.message(Page.menu)
+async def get_menu(msg: types.Message):
+    await msg.answer(App.MENU, reply_markup=App.menu())
 
 # @router.message(F.text, Form.logined)
 # async def start_questionnaire3(msg: types.Message, state: FSMContext):
