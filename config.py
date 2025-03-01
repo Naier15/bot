29155bfcdbe9f9c6cd4144ceadccb2ca10e8e4cb
Roman <grv1510@mail.ru>
2025-03-01 13:51:25 +1000

    added env config in readme

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass
 from os import environ
-
 from dotenv import load_dotenv
 
 
-# res = load_dotenv()
+res = load_dotenv()
 
 @dataclass
 class Config:
-    BOT_TOKEN = environ.get('BOT_TOKEN', '1291275643:AAHIk28uq57pVT5ZZz-IEQllgQhP5_mwx7s')
+    BOT_TOKEN = environ['BOT_TOKEN']
     MANAGER_BOT_LINK = environ.get('MANAGER_BOT_LINK', 'https://t.me/bashnipro_bot')
     DJANGO_HOST = environ.get('DJANGO_HOST', 'https://bashni.pro/')
     REGION = environ.get('REGION', 'Asia/Vladivostok')
