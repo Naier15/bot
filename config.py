e8c04126d7fb1682d68ab86ff1a7e85fe1b67579
Roman <grv1510@mail.ru>
2025-03-14 18:53:20 +1000

    fix

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     DJANGO_PATH = environ['DJANGO_PATH']
     LOG_FILE = environ.get('LOG_FILE', 'log.log')
     MANAGER_LINK = environ.get('MANAGER_LINK', '+79679580207')
-    DJANGO_HOST = environ.get('DJANGO_HOST', 'https://bashni.pro/')
+    DJANGO_HOST = environ.get('DJANGO_HOST', 'https://bashni.pro')
     REGION = environ.get('REGION', 'Asia/Vladivostok')
     DEBUG = bool(environ.get('DEBUG', 0))
     DISPATCH_TIME = environ.get('DISPATCH_TIME', '16:20')
